af20636f2a9cde804a1512a1c10989e9e2e02720
Anderson Resendes da Silva <andersonresendesdasilva@MacBook-Pro-de-Anderson.local>
2019-11-02 17:31:50 -0300

    criptografar e descriptografar

--- src/Key.java
+++ src/Key.java
@@ -17,60 +17,90 @@ public class Key {
     }
 
     public boolean generation() throws Exception {
-        System.out.println("Comecou!!");
-        //Gerar p
-        BigInteger p = PrimeGenerator.getPrimeNumber(this.length);
+        String s = "a";
+        while (s.equals("a")) {
+            System.out.println("Comecou!!");
+            //Gerar p
+            BigInteger p = PrimeGenerator.getPrimeNumber(this.length);
 
-        //Gerar q
-        BigInteger q = PrimeGenerator.getPrimeNumber(this.length);
+            //Gerar q
+            BigInteger q = PrimeGenerator.getPrimeNumber(this.length);
 
-        //Gerar N
-        BigInteger N = p.multiply(q);
-        //Gerar e
-        BigInteger totient = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+            //Gerar N
+            BigInteger N = p.multiply(q);
 
-        BigInteger e =  relativePrime(totient, this.length*2);
+            //Gerar (p-1)(q-1)
+            BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
 
-        BigInteger d = euclidesExt(e, totient, BigInteger.ONE);
+            //Gerar e
+            BigInteger e = relativePrime(phi);
 
-        if (e.equals(d))
-            System.out.println(e);
+            //Gerar d
+            BigInteger d = extendedGcd(e, phi);
 
-        Properties publicKey = new Properties();
-        publicKey.setProperty("n", N.toString());
-        publicKey.setProperty("e", e.toString());
-        FileOutputStream publicKeyFile = new FileOutputStream(this.publicKey);
-        publicKey.store(publicKeyFile, null);
+            Properties publicKey = new Properties();
+            publicKey.setProperty("n", N.toString());
+            publicKey.setProperty("e", e.toString());
+            FileOutputStream publicKeyFile = new FileOutputStream(this.publicKey);
+            publicKey.store(publicKeyFile, null);
 
-        Properties privateKey = new Properties();
-        privateKey.setProperty("n", N.toString());
-        privateKey.setProperty("d", d.toString());
-        FileOutputStream privateKeyFile = new FileOutputStream(this.privateKey);
-        publicKey.store(privateKeyFile, null);
+            Properties privateKey = new Properties();
+            privateKey.setProperty("n", N.toString());
+            privateKey.setProperty("d", d.toString());
+            FileOutputStream privateKeyFile = new FileOutputStream(this.privateKey);
+            publicKey.store(privateKeyFile, null);
+
+
+            BigInteger x = new BigInteger("a".getBytes());
+            BigInteger y = x.modPow(e, N);
+            BigInteger m = y.modPow(d, N);
+            s = new String(m.toByteArray());
+            System.out.println(s);
+        }
 
         return true;
 
     }
 
-    BigInteger euclidesExt(BigInteger a, BigInteger b, BigInteger c)
-    {
-        BigInteger r;
+    BigInteger extendedGcd(BigInteger a, BigInteger b) {
+        BigInteger s = BigInteger.ZERO;
+        BigInteger t = BigInteger.ONE;
+        BigInteger r = b;
+        BigInteger oldS = BigInteger.ONE;
+        BigInteger oldT = BigInteger.ZERO;
+        BigInteger oldR = a;
 
-        r = b.mod(a);
+        while (!r.equals(BigInteger.ZERO)) {
+            BigInteger quotient = oldR.divide(r);
+            BigInteger calcOldR = oldR.subtract(quotient.multiply(r));
+            BigInteger calcOldS = oldS.subtract(quotient.multiply(s));
+            BigInteger calcOldT = oldT.subtract(quotient.multiply(t));
 
-        if (r.equals(BigInteger.ZERO)) {
-            return c.divide(a).mod(b.divide(a));	// retorna (c/a) % (b/a)
+            oldR = r;
+            oldS = s;
+            oldT = t;
+
+            r = calcOldR;
+            s = calcOldS;
+            t = calcOldT;
         }
 
-        return ((euclidesExt(r, a, c.negate()).multiply(b).add(c)).divide(a.mod(b)));
+        return oldS;
+
     }
 
-    private BigInteger relativePrime(BigInteger totient, int bits) {
-        BigInteger e = new BigInteger(bits, new Random());
-        while (e.gcd(totient).compareTo(BigInteger.ONE) != 0) {
-            e = e.add(BigInteger.ONE);
+    private BigInteger relativePrime(BigInteger phi) {
+        BigInteger e = new BigInteger(phi.bitLength(), new Random());
+        while (!gcd(e, phi).equals(BigInteger.ONE)) {
+            e = new BigInteger(phi.bitLength(), new Random());
         }
         return e;
+    }
+
+    public BigInteger gcd(BigInteger a, BigInteger b) {
+        if (b.equals(BigInteger.ZERO))
+            return a;
+        return gcd(b, a.mod(b));
     }
 
     public static void main(String[] args) throws Exception {
