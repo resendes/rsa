d7da3f86952272aa07eff8698364bb8d674d6875
Anderson Resendes da Silva <andersonresendesdasilva@MacBook-Pro-de-Anderson.local>
2019-11-04 00:12:53 -0300

    encrypt and decrypt

--- src/Key.java
+++ src/Key.java
@@ -1,68 +1,46 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Properties;
-import java.util.Random;
+import java.util.*;
 
 public class Key {
 
-    int length;
-    String publicKey;
-    String privateKey;
+    public static void generate(int length, String publicKey, String privateKey) throws Exception {
+        //Gerar p
+        BigInteger p = PrimeGenerator.getPrimeNumber(length);
 
-    public Key(int length, String publicKey, String privateKey) {
-        this.length = length;
-        this.publicKey = publicKey;
-        this.privateKey = privateKey;
-    }
+        //Gerar q
+        BigInteger q = PrimeGenerator.getPrimeNumber(length);
 
-    public boolean generation() throws Exception {
-        String s = "a";
-        while (s.equals("a")) {
-            System.out.println("Comecou!!");
-            //Gerar p
-            BigInteger p = PrimeGenerator.getPrimeNumber(this.length);
+        //Gerar N
+        BigInteger N = p.multiply(q);
 
-            //Gerar q
-            BigInteger q = PrimeGenerator.getPrimeNumber(this.length);
+        //Gerar (p-1)(q-1)
+        BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
 
-            //Gerar N
-            BigInteger N = p.multiply(q);
+        //Gerar e
+        BigInteger e = relativePrime(phi);
 
-            //Gerar (p-1)(q-1)
-            BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        //Gerar d
+        BigInteger d = extendedGcd(e, phi);
 
-            //Gerar e
-            BigInteger e = relativePrime(phi);
+        Properties publicKeyProp = new Properties();
+        publicKeyProp.setProperty("n", N.toString());
+        publicKeyProp.setProperty("e", e.toString());
+        FileOutputStream publicKeyFile = new FileOutputStream(publicKey);
+        publicKeyProp.store(publicKeyFile, null);
 
-            //Gerar d
-            BigInteger d = extendedGcd(e, phi);
-
-            Properties publicKey = new Properties();
-            publicKey.setProperty("n", N.toString());
-            publicKey.setProperty("e", e.toString());
-            FileOutputStream publicKeyFile = new FileOutputStream(this.publicKey);
-            publicKey.store(publicKeyFile, null);
-
-            Properties privateKey = new Properties();
-            privateKey.setProperty("n", N.toString());
-            privateKey.setProperty("d", d.toString());
-            FileOutputStream privateKeyFile = new FileOutputStream(this.privateKey);
-            publicKey.store(privateKeyFile, null);
-
-
-            BigInteger x = new BigInteger("a".getBytes());
-            BigInteger y = x.modPow(e, N);
-            BigInteger m = y.modPow(d, N);
-            s = new String(m.toByteArray());
-            System.out.println(s);
-        }
-
-        return true;
+        Properties privateKeyProp = new Properties();
+        privateKeyProp.setProperty("n", N.toString());
+        privateKeyProp.setProperty("d", d.toString());
+        FileOutputStream privateKeyFile = new FileOutputStream(privateKey);
+        privateKeyProp.store(privateKeyFile, null);
 
     }
 
-    BigInteger extendedGcd(BigInteger a, BigInteger b) {
+    private static BigInteger extendedGcd(BigInteger a, BigInteger b) {
         BigInteger s = BigInteger.ZERO;
         BigInteger t = BigInteger.ONE;
         BigInteger r = b;
@@ -89,7 +67,7 @@ public class Key {
 
     }
 
-    private BigInteger relativePrime(BigInteger phi) {
+    private static BigInteger relativePrime(BigInteger phi) {
         BigInteger e = new BigInteger(phi.bitLength(), new Random());
         while (!gcd(e, phi).equals(BigInteger.ONE)) {
             e = new BigInteger(phi.bitLength(), new Random());
@@ -97,15 +75,62 @@ public class Key {
         return e;
     }
 
-    public BigInteger gcd(BigInteger a, BigInteger b) {
+    private static BigInteger gcd(BigInteger a, BigInteger b) {
         if (b.equals(BigInteger.ZERO))
             return a;
         return gcd(b, a.mod(b));
     }
 
+    public static List<BigInteger> bruteForcePublicKey() throws IOException {
+        FileInputStream publicKeyFile = new FileInputStream("rsa_public.txt");
+        Properties publicKey = new Properties();
+        publicKey.load(publicKeyFile);
+
+        BigInteger n = new BigInteger(publicKey.getProperty("n"));
+
+        List<BigInteger> factors = new LinkedList<>();
+
+        while(n.mod(BigInteger.TWO).compareTo(BigInteger.ZERO) == 0){
+            factors.add(BigInteger.TWO);
+            n = n.divide(BigInteger.TWO);
+        }
+
+        BigInteger d = BigInteger.valueOf(3); // Possíveis fatores
+        BigInteger d2 = BigInteger.valueOf(9); // d2 = d * d
+        while(d2.compareTo(n) <= 0){
+            // Se d é fator, faz a divisão e armazena o fator
+            if(n.mod(d).equals(BigInteger.ZERO)) {
+                factors.add(d);
+                n = n.divide(d);
+            }else{
+                // Se d não é fator, verifica o próximo
+                d = d.add(BigInteger.TWO);
+                d2 = d.multiply(d); // d2 = d*d
+            }
+        }
+        // Essa condição é necessária quando n for primo
+        if(n.compareTo(BigInteger.ONE) > 1){
+            factors.add(n);
+        }
+        return factors;
+    }
+
+    private static boolean contains(String[] args, String element) {
+        return Arrays.stream(args).anyMatch(element::equals);
+    }
+
     public static void main(String[] args) throws Exception {
-        Key key = new Key(1024,"rsa_public.txt", "rsa_private.txt");
-        key.generation();
+        if (contains(args, "generate")) {
+            Key.generate(10, "rsa_public.txt", "rsa_private.txt");
+        }
+        if (contains(args, "encrypt_decrypt")) {
+            Rsa.encrypt();
+            Rsa.decrypt();
+        }
+        if (contains(args, "brute_force")) {
+            Key.bruteForcePublicKey().stream().forEach(x -> System.out.println(x.toString()));
+        }
+
     }
 
 
